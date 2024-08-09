db25f00c914ff2089f77cd3601e5f367026056f1
Yerassyl Maikhanov <yerassyl.maikhanov@e-magnum.kz>
2024-08-09 21:54:51 +0500

    add

---
+++ templates/feature/data/data_storage.dart
@@ -0,0 +1,9 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class {{name}}StorageImlementation extends {{name}}Storage {
+final SharedPreferences sharedPreferences;
+
+  {{name}}StorageImlementation(this.sharedPreferences);
+
+
+}
---
+++ templates/feature/domain/data_repository.dart
@@ -0,0 +1,9 @@
+import 'package:injilkz_core/core.dart';
+
+class {{name}}RepositoryImlementation extends {{name}}Repository {
+  final InjilApiClient _apiClient;
+
+  {{name}}RepositoryImlementation(this._apiClient);
+
+
+}
